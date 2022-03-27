cc7e70c074842ec1a7f6cd244bce81f5549b7402
Allen-Myfx <allen@myfxmarkets.com>
2022-03-27 23:44:42 +1100

    init

--- src/main.js
+++ src/main.js
@@ -3,4 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// import Element from 'element-ui'
+// import "element-ui/lib/theme-chalk/index.css"
+// App.use(Element)
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// App.use(ElementPlus)
+
+import axios from 'axios'
+
+
+const app = createApp(App).use(ElementPlus).use(store).use(router)
+app.config.globalProperties.axios = axios
+
+
+app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,63 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Blogs from '../views/Blogs.vue'
+import BlogEdit from '../views/BlogEdit.vue'
+import BlogDetail from '../views/BlogDetail.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    // {
+    //     path: '/home',
+    //     name: 'home',
+    //     component: HomeView
+    // },
+    // {
+    //     path: '/about',
+    //     name: 'about',
+    //     // route level code-splitting
+    //     // this generates a separate chunk (about.[hash].js) for this route
+    //     // which is lazy-loaded when the route is visited.
+    //     component: () =>
+    //         import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    // },
+
+    // my stuff
+    {
+        path: '/',
+        name: 'Index',
+        redirect: { name: "Blogs" }
+    },
+    {
+        path: '/blogs',
+        name: 'Blogs',
+        component: Blogs
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/blog/add',
+        name: 'BlogEdit',
+        component: BlogEdit
+    },
+    {
+        path: '/blog/:blogId',
+        name: 'BlogDetail',
+        component: BlogDetail
+    },
+    {
+        path: '/blogs/:blogId/edit',
+        name: 'BlogEdit',
+        component: BlogEdit
+    }
+
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
